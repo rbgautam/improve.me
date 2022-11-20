ac79e219e8aaa475e19ae703df2a632ddb4b22cb
rbgautam <rahulbgautam@gmail.com>
2022-11-20 12:24:04 -0600

    Added google Auth endpoint

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 This is a demo project showcasing use of React.js+Express+MongoDb
 
 This is a mock website for getting user feedback after emails with link to survey are sent to them.
+
+Hosted on Heroku:
+https://improve-me-forgeinnovations.herokuapp.com/
+
+Libraries usued;
+Express : API BackEnd
+Passpost : OAuth 2.0 middleware
+Passport-Goooogle-OAuth20: Google passport strategy
+Nodemon: Automatically restart local server (node run dev) 
--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/',(req,res)=> {
-    res.send({hi:'there'});
+// Google Oauth
+const GOOGLE_CLIENT_ID = keys.googleClientId || process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET =keys.googleClientSecret || process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   //return cb(err, user);
+    //   console.log('user profile',user);
+    // });
+    console.log('accesstoken:',accessToken);
+    console.log('refreshToken:',refreshToken);
+    console.log('profile:',profile);
+  }
+
+));
+
+app.get('/auth/google',
+    passport.authenticate('google',{scope:['profile','email']})
+);
+app.get('/home',(req,res)=>{
+    res.send({user:'Loogged in success'});
 });
+app.get('/login',(req,res)=>{
+    res.send({user:'Loogged in success'});
+});
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/home');
+  });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
