736912410a2ef6d5be80aa619b0de003a43a1858
rbgautam <rahulbgautam@gmail.com>
2022-11-21 11:21:44 -0600

    Add condition for switching between Local and Heroku

--- services/passportService.js
+++ services/passportService.js
@@ -1,12 +1,16 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
-
+//
+let GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+let GOOGLE_CLIENT_SECRET =process.env.GOOGLE_CLIENT_SECRET;
 // Google Oauth
-const GOOGLE_CLIENT_ID = keys.googleClientId || process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET =keys.googleClientSecret || process.env.GOOGLE_CLIENT_SECRET;
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// const GOOGLE_CLIENT_SECRET =process.env.GOOGLE_CLIENT_SECRET;
+if (!GOOGLE_CLIENT_ID) {
+    const keys = require('../config/keys');
+    GOOGLE_CLIENT_ID = keys.googleClientId || process.env.GOOGLE_CLIENT_ID;
+    GOOGLE_CLIENT_SECRET =keys.googleClientSecret || process.env.GOOGLE_CLIENT_SECRET;    
+}
+
+// 
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
